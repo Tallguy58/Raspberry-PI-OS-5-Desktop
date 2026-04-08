292fbd4ca5714e9551b5b259cba869481db5a934
Alexander Gent <aagent298@gmail.com>
2026-04-08 19:59:07 +1000

    Add files via upload

---
+++ cinnamon.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+currentuser=$(users | awk '{print $1}')
+
+install-package() {
+if ! dpkg -s $1 >/dev/null 2>&1; then
+    echo -e '\033[1;33mInstalling \033[1;32m'$1' \033[0m\c'
+    apt-get -y -qq install $1 >/dev/null
+    echo -e '\033[1;36m... OK\033[0m'
+fi
+}
+
+install-package cinnamon-desktop-environment
+install-package lightdm-gtk-greeter-settings
+install-package cinnamon
+update-alternatives --set x-session-manager /usr/bin/cinnamon-session >/dev/null
+sed -i 's/.*autologin-user=.*/autologin-user='$currentuser'/' /etc/lightdm/lightdm.conf
+sed -i 's/.*greeter-session=pi-greeter.*/greeter-session=lightdm-gtk-greeter/' /etc/lightdm/lightdm.conf
+sed -i 's/.*xserver-command=X.*/xserver-command=X -s 0 -dpms/' /etc/lightdm/lightdm.conf
+
+apt-get -y -qq update >/dev/null
+apt-get -y -qq upgrade >/dev/null
+
+shutdown -r now
